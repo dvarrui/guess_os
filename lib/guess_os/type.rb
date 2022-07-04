044528e0c36491da920ebebca34c69fc9696e6b3
David Vargas <dvarrui@protonmail.com>
2022-07-04 12:30:06 +0100

    [U] .

--- lib/guess_os/type.rb
+++ lib/guess_os/type.rb
@@ -5,6 +5,16 @@ require_relative 'os'
 module GuessOS
   module Type
     def self.guess(host)
+      os = guess_gnulinux(host)
+      return os unless os.type == :unkown
+
+      os = guess_minix(host)
+      return os unless os.type == :unkown
+
+      os
+    end
+
+    def self.guess_gnulinux(host)
       command = 'lsb_release -d'
       conn = GuessOS::Conn.new(host)
 
@@ -19,17 +29,30 @@ module GuessOS
       OS.new(type, name, desc)
     end
 
-    def revise_guess(host)
+    def self.guess_minix(host)
+      command = 'cat /etc/motd |grep MINIX'
+      conn = GuessOS::Conn.new(host)
+
+      conn.exec(command)
+      return OS.new(:unkown, :unkown, conn.status) unless conn.ok
+
+      output = conn.last_output
+      items = output.split
+      type =  'minux'
+      name =  items[7]&.downcase
+      desc =  output
+      OS.new(type, name, desc)
+    end
+
+    def reviseguess(host)
       commands = {
         win10: 'pwd',
         winserver: 'echo %windir%',
-        minix: 'cat /etc/motd |grep MINIX'
       }
 
       position = {
         win10: 0,
         winserver: 0,
-        minix: 7
       }
     end
   end
