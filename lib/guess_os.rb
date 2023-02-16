33b5b855173af84aa49d3f142369f9755b7cfddd
dvarrui <dvarrui@protonmail.com>
2023-02-16 05:25:45 +0000

    U

--- lib/guess_os.rb
+++ lib/guess_os.rb
@@ -3,5 +3,17 @@
 require_relative "guess_os/version"
 require_relative "guess_os/host"
 
+##
+# Usage:
+#
+# host = GuessOS::Host.new(
+#   ip: ip,
+#   port: port,
+#   username: username,
+#   password: password
+# )
+#
+# => host.os
+
 module GuessOS
 end
